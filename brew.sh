094b418cf4eec357587e13e6c03ffa29e455279c
Kyle Rubenok <kyle@rubenok.ca>
2018-07-23 20:43:07 -0700

    Tweaks after testing

--- brew.sh
+++ brew.sh
@@ -21,17 +21,9 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade --all
 
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
-# Install zsh.
-brew install zsh
-
-brew tap homebrew/versions
-brew tap caskroom/fonts
+brew tap caskroom/versions
 brew tap caskroom/cask
+brew tap caskroom/fonts
 
 # We installed the new shell, now we have to activate it
 echo "Adding the newly installed shell to the list of allowed shells"
@@ -52,8 +44,8 @@ brew install git-extras
 brew install tree
 brew install dockutil
 brew install git
-# brew install mas
 # Commenting out mas because of Mojave beta
+# brew install mas
 brew install zsh
 brew install z
 
@@ -68,16 +60,13 @@ brew cask install figma
 brew cask install firefox
 brew cask install franz
 brew cask install geekbench
-brew cask install gfxcardstatus
 brew cask install github-beta
 brew cask install google-backup-and-sync
 brew cask install google-chrome
 brew cask install highsierramediakeyenabler
 brew cask install hyper
 brew cask install iina
-brew cask install intel-power-gadget
 brew cask install java
-brew cask install jetbrains-toolbox
 brew cask install notion
 brew cask install private-internet-access
 # brew cask install setapp
@@ -91,7 +80,7 @@ brew cask install font-fira-code
 brew cask install font-hind
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package
 
 # Remove outdated versions from the cellar.
 brew cleanup
--- web.sh
+++ web.sh
@@ -23,8 +23,6 @@ brew install node
 # Remove outdated versions from the cellar.
 brew cleanup
 
-npm install -g coffee-script
-npm install -g grunt-cli
 npm install -g jshint
 npm install -g less
 
