acc3443a3009c8476c7a7eab640c20106fbc512e
Kyle Rubenok <kyle@rubenok.ca>
2020-05-01 20:47:18 -0700

    First run was a pass!

--- zshrc.sh
+++ zshrc.sh
@@ -1,7 +1,29 @@
 source /usr/local/share/antigen/antigen.zsh
 
 # Load Antigen configurations
-antigen init ~/.antigenrc
+antigen init ~/.dotfiles/antigenrc
+
+# ===================================
+# ------------zsh options------------
+# ===================================
+
+setopt list_types                   # show types in completion
+setopt hist_ignore_dups             # ignore duplication command history list
+setopt hist_verify                  # expand history onto the current line instead of executing it
+setopt hist_expire_dups_first       # remove oldest duplicate commands from the history first
+setopt hist_ignore_space            # don't save commands beginning with spaces to history
+setopt inc_append_history           # always be saving history (not just when the shell exits)
+setopt auto_menu                    # show completion menu on succesive tab press
+setopt auto_pushd                   # make cd push the old directory onto the directory stack
+
+# history
+HISTFILE=$HOME/.dotfiles/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+# z menu style
+zstyle ':completion:*' menu select
+ZSHZ_DATA=$HOME/.dotfiles/z
 
 # ===============================
 # ------------Aliases------------
@@ -34,7 +56,7 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 # them
 # =============================
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
 
